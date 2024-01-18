68d3aa02ffac5718c5350d8b38a74121fd0d8b4e
Robin Gunnarsson <robin.gunnarsson@etteplan.com>
2024-01-18 22:37:01 +0100

    Created connect 4 board

--- src/main/resources/static/script/connect4.js
+++ src/main/resources/static/script/connect4.js
@@ -6,17 +6,95 @@ const makeMove = (player) => {
     //make a move for the player
 }
 
-const setupGame = () => {
+const setupGame = (gameArea) => {
     //Create new empty game board of 7 columns × 6 rows
+    const gameAreaContent = `
+        <button class="connect4Close">
+            <i class="bi bi-x-circle"></i>
+        </button>
+        <table class="connect4Board">
+            <thead>
+                <tr>
+                    <th><span/></th>
+                    <th><span/></th>
+                    <th><span/></th>
+                    <th><span/></th>
+                    <th><span/></th>
+                    <th><span/></th>
+                    <th><span/></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                </tr>
+                <tr>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                </tr>
+                <tr>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                </tr>
+                <tr>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                </tr>
+                <tr>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                </tr>
+                <tr>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td>
+                    <td><span/></td> 
+                    <td><span/></td>
+                    <td><span/></td>
+                </tr>
+            </tbody>
+        </table> 
+    `;
+
+    gameArea.innerHTML = gameAreaContent;
+    gameArea.querySelector("button.connect4Close").addEventListener("click", () => {
+        console.log("Close button");
+        gameArea.parentElement.removeChild(gameArea);
+    });
 }
 
 function clickIcon(e) {
     const gameArea = document.createElement("div");
-    console.log("create game area");
     gameArea.classList.add("connect4Area");
     gameArea.classList.add("gameArea")
 
-    console.log(e.pageX + " : " + e.pageY);
     gameArea.style.left = e.pageX + "px";
     gameArea.style.top = e.pageY + "px";
 
@@ -27,6 +105,8 @@ function clickIcon(e) {
     gameArea.style.height = mainElement.offsetHeight + "px";
     gameArea.style.top = mainElement.offsetTop + "px";
     gameArea.style.left = mainElement.offsetLeft + "px";
+
+    setupGame(gameArea);
 }
 
 function connect4Init() {
